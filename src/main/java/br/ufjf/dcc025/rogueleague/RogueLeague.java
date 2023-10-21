8d1ffcf232028b41944b72dc226f58d85abb8ce4
B4Hive <b4.bruno.santos@gmail.com>
2023-10-20 22:20:50 -0300

    Initial commit

---
+++ src/main/java/br/ufjf/dcc025/rogueleague/RogueLeague.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufjf.dcc025.rogueleague;
+
+/**
+ *
+ * @author Bruno dos Santos Silva - 201935031
+ */
+public class RogueLeague {
+
+    public static void main(String[] args) {
+        //
+    }
+}
