86b8c8780a8700fcf2014012b4f4f61d16db3343
B4Hive <b4.bruno.santos@gmail.com>
2024-03-22 16:53:58 -0300

    0.0
    Planning done;
    Plan will be passed to readme file;

---
+++ src/main/java/beehive/rogueleague/RogueLeague.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package beehive.rogueleague;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author BeeHive
+ */
+public class RogueLeague {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "This will be a game someday...", "RogueLeague", 0);
+    }
+    
+}
